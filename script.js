166a7541c2cba3a48f9b91d253cd177edf59a9e8
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2020-11-06 19:20:47 +0200

    izmaina

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Virsraksts</title>
-    <link href="style.css" rel="stylesheet" />
-  </head>
-  <body>
-
-    <div class="wrapper">
-
-    </div>
-
-<script src="script.js"></script>
-
-</body>
-</html>
+alert(1);
