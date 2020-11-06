767501b5442405d1ff3a2a60942cbcec4a863fe1
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2020-11-06 23:42:15 +0200

    aa

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert(1);
+let tabula=[
+    {
+    vards:'tenis',
+    vecums:24,
+    'sapņo': true
+    },
+    {
+    vards:'elza',
+    vecums:27,
+    'sapņo': false
+    },
+    {
+    vards:'ilga',
+    vecums:31,
+    'sapņo': true
+    }
+    ];
+    let rindas=document.querySelector('.rindas');
+    rindas.innerHTML=tabula[1]['vards'];
